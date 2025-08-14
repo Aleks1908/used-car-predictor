0e4e67111635fdf61e9f7f06a7a79c73ac7ed9fc
Aleks4o <aleks4o@Aleks4os-MacBook-Pro.local>
2025-08-14 18:57:50 +0300

    inital struct

---
+++ used-car-predictor/Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapGet("/api/hello", () => Results.Ok(new { message = "Hello from .NET 9!" }));
+
+app.MapFallbackToFile("index.html");
+
+app.Run();
