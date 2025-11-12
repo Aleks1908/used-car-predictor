55cd890fb087a96f9cd52360f70f72ee5ec65e72
Aleks1908 <al.ivanov1908@gmail.com>
2025-11-12 22:39:58 +0200

    tests wip

---
+++ used-car-predictor/used-car-predictor.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+using Assert = Xunit.Assert;
+
+namespace used_car_predictor.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Sanity()
+    {
+        Assert.True(1 + 1 == 2);
+    }
+}
